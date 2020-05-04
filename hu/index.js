53661216d7df9d10b30f37bb083594d7b9c371a4
is703374@iteso.mx <is703374@iteso.mx>
2020-05-03 22:36:55 -0500

    commit

--- hu/index.js
+++ hu/index.js
@@ -71,5 +71,5 @@ app.get('/bitacoras', function (req, res) {
   //res.end('hola mundo');
   res.sendFile(__dirname + '/public/views/bitacoras.html');
 });
-app.listen(80);
+app.listen(process.env.PORT||80);
 
